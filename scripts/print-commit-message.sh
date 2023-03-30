046adfd59db1366cac48e43ac5c49852225a124f
Mario Bezerra <mario.bezerra@incognia.com>
2023-03-29 18:07:03 -0700

    feat: improve commit message body
    Signed-off-by: Mario Bezerra <mario.bezerra@incognia.com>

--- scripts/print-commit-message.sh
+++ scripts/print-commit-message.sh
@@ -10,26 +10,25 @@ then
     LAST_UPDATE_COMMIT_HASH="HEAD"
 fi
 
-PR_COMMIT_HASHES="$(git log --merges --format=%H HEAD...${LAST_UPDATE_COMMIT_HASH})"
-HAS_MERGE_COMMIT=true
+COMMIT_HASHES="$(git log --merges --format=%H HEAD...${LAST_UPDATE_COMMIT_HASH})"
+IS_MERGE_COMMIT=true
 
 # If does not exist, commits were merged via squash or commits were pushed directly to branch
-if [[ -z "${PR_COMMIT_HASHES}" ]]
+if [[ -z "${COMMIT_HASHES}" ]]
 then
-    PR_COMMIT_HASHES="$(git log --format=%H HEAD...${LAST_UPDATE_COMMIT_HASH})"
-    HAS_MERGE_COMMIT=false
+    COMMIT_HASHES="$(git log --format=%H HEAD...${LAST_UPDATE_COMMIT_HASH})"
+    IS_MERGE_COMMIT=false
 fi
 
 printf "chore(k8s): update images to version ${IMAGE_TAG}\n\n"
 
-for PR_COMMIT_HASH in ${PR_COMMIT_HASHES}
+for COMMIT_HASH in ${COMMIT_HASHES}
 do
-    if [[ ${HAS_MERGE_COMMIT} = true ]]
+    if [[ ${IS_MERGE_COMMIT} = true ]]
     then
-        # Get commit subject
-        git log --format=%b -n1 ${PR_COMMIT_HASH} | cat
+        PR_REFERENCE="$(git log --format=%s -n1 ${COMMIT_HASH} | grep -oE "#[0-9]+")"
+        printf "$(git log --format=%b -n1 ${COMMIT_HASH}) (${PR_REFERENCE})\n"
     else
-        # Get commit message
-        git log --format=%s -n1 ${PR_COMMIT_HASH} | cat
+        git log --format=%s -n1 ${COMMIT_HASH} | cat
     fi
 done
